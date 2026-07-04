7ea82ccf62f4410b22ade85b0a9eecc71b9bda47
AldairQF <aldair.quispe@ms4m.com>
2026-07-03 22:03:15 -0400

    refactor(ui): drop dead time-filter wiring from CAN ID panel
    show_time_filter and time_config_vm were left unused after the time
    filter moved out of the side panel in the earlier UI audit.

--- views/realtime_analysis_window_manager.py
+++ views/realtime_analysis_window_manager.py
@@ -5,16 +5,14 @@ from views.realtime_analysis_window import RealTimeAnalysisWindow
 
 
 class RealTimeAnalysisWindowManager(BaseWindowManager):
-    def __init__(self, *, analysis_vm, dbc_manager, time_config_vm, parent=None):
+    def __init__(self, *, analysis_vm, dbc_manager, parent=None):
         super().__init__()
         self._analysis_vm = analysis_vm
         self._dbc_manager = dbc_manager
-        self._time_config_vm = time_config_vm
 
     def _create_window(self) -> RealTimeAnalysisWindow:
         return RealTimeAnalysisWindow(
             self._analysis_vm,
             self._dbc_manager,
-            self._time_config_vm,
             parent=None,
         )
